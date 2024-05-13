88b43312a52dabfdfdad23a9efed6717e1b2c524
Leonardo do Prado <leonardodoprado231@gmail.com>
2024-05-13 19:56:05 -0300

    att 13/05

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
 from routes.user import user_route
 from database.database import db
+from flask_login import LoginManager
 
 def configure_all(app):
     configure_routes(app)
-    init_db(app)
+    configure_db(app)
 
 def configure_routes(app):
     app.register_blueprint(user_route, url_prefix='/user')
 
-def init_db(app):
+def configure_db(app):
     app.config['SECRET_KEY'] = "your-secret-key"
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:senha123@localhost/controlwise'
     db.init_app(app)
+    login_manager = LoginManager()
+    login_manager.init_app(app)
--- routes/user.py
+++ routes/user.py
@@ -1,32 +1,21 @@
 from flask import Blueprint, request, jsonify
-from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required, login_manager
 import bcrypt
+from database.database import db
 from database.models.user import User
 
 user_route = Blueprint('user', __name__)
 
-@user_route.route('/login', methods=["POST"])
-def login():
-    data = request.json
-    username = data.get("username")
-    password = data.get("password")
-
-    if username and password:
-        user = User.query.filter_by(username=username).first()
-        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
-            login_user(user)
-            print(current_user.is_authenticated)
-            return jsonify({"message": "Autenticação realizada com sucesso!"})
+@user_route.route("/<int:id_user>", methods=["GET"])
+#@login_required
+def read_user(id_user):
+    user = User.query.get(id_user)
+    if user:
+        return{"username": user.username, "perfil": user.role}
     
-    return jsonify({"message": "Credenciais Inválidas!"}), 400
+    return jsonify({"message": "Usuário não encontrado!"}), 404
 
-@user_route.route("/logout", methods=["GET"])
-@login_required
-def logout():
-    logout_user()
-    return jsonify({"message": "Logout realizado com sucesso!"})
-
-@user_route.route("/user", methods=["POST"])
+@user_route.route("/", methods=["POST"])
 def create_user():
     data = request.json
     username = data.get("username")
@@ -40,47 +29,3 @@ def create_user():
         return jsonify({"message": "Usuário cadastrado com sucesso!"})
     
     return jsonify({"message": "Dados Inválidos!"}), 400
-
-@user_route.route("/user/<int:id_user>", methods=["GET"])
-@login_required
-def read_user(id_user):
-    user = User.query.get(id_user)
-    if user:
-        return{"username": user.username, "perfil": user.role}
-    
-    return jsonify({"message": "Usuário não encontrado!"}), 404
-    
-@user_route.route("/user/<int:id_user>", methods=["PUT"])
-@login_required
-def update_user(id_user):
-    data = request.json
-    user = User.query.get(id_user)
-
-    if id_user != current_user.id and current_user.rule == "user":
-        return jsonify({"message": "Operação não permitida para o nível de usuário!"}), 403
-
-    if user and data.get("password"):
-        user.password = data.get("password")
-        db.session.commit()
-        return jsonify({"message": f"Senha do usuário '{id_user}-{user.username}' atualizada com sucesso!"})
-    
-    return jsonify({"message": "Usuário não encontrado!"}), 404
-
-@user_route.route("/user/<int:id_user>", methods=["DELETE"])
-@login_required
-def delete_user(id_user):
-    user = User.query.get(id_user)
-
-    if current_user.role != "admin":
-        return jsonify({"message": "Operação não permitida para o nível de Usuário!"}), 403
-
-
-    if id_user == current_user.id:
-        return jsonify({"message": "Usuário logado não pode Deletar a si mesmo!"}), 403
-
-    if user:
-        db.session.delete(user)
-        db.session.commit()
-        return jsonify({"message": f"Usuário '{id_user}-{user.username}' deletado com sucesso!"})
-    
-    return jsonify({"message": "Usuário não encontrado!"}), 404
