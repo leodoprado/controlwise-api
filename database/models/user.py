25952659329db37ab0fb55d139abe405e4f8076b
Leonardo do Prado <leonardodoprado231@gmail.com>
2024-07-13 15:46:38 -0300

    refactor

--- database/models/user.py
+++
@@ -1,8 +0,0 @@
-from database.database import db
-from flask_login import UserMixin
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False, unique=True)
-    email = db.Column(db.String(80), nullable=False)
-    password = db.Column(db.String(80), nullable=False)
