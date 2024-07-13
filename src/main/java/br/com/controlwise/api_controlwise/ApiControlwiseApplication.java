d71e23247561c39ea95f28b5ac07ffd89c5e772b
Leonardo do Prado <leonardodoprado231@gmail.com>
2024-07-13 15:43:44 -0300

    feat: first commit

---
+++ src/main/java/br/com/controlwise/api_controlwise/ApiControlwiseApplication.java
@@ -0,0 +1,13 @@
+package br.com.controlwise.api_controlwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControlwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControlwiseApplication.class, args);
+	}
+
+}
